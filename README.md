2bddfe9c23f7b95d6188575f4e4069fae0ee7ba6
charles <charles@charless-MacBook-Pro.local>
2017-01-15 12:57:14 -0600

    testing in progress

--- README.md
+++ README.md
@@ -1,34 +1,18 @@
-# Prime Group jQuery Challenge
-jQuery is great! It allows us to do so many things! You and your group will need to flex everything you know about 
-Javascript, jQuery, and CSS to tackle this challenge. 
 
-## The Fruit Market
-For this challenge, you will be working with 4 commodities; 
-Apples, Oranges, Bananas, and Grapes. Delicious, right?
+## The Fruit Market##
 
-When the application loads, you will need to have information for each of the commodities, specifically the name 
-and the ‘market price’ of each. This information will need to be displayed in a meaningful way on the DOM.
+* *Authors*
+* *Tyler Herman*
+* *Bob Hinkley*
+* *Charles Kuruvila*
+* *Date: 01/15/2017*
 
-Every 15 seconds, the prices should change however, and with it, the listed price on the DOM. Specifically, 
-the market price of each of the items should fluctuate up or down 50 cents (between 1 cent and 50 cents) with each 15 second interval. 
-Any given fruit is not allowed to go below a cost of 50 cents, or above the cost of 9 dollars and 99 cents. 
 
-Available to the user is a ‘total cash’ and an inventory display that shows how much of each of the fruits they 
-have purchased. Also in the user display, should be an ‘average purchased price’, which shows, on average, how 
-much money they have spent on a given fruit in their inventory.
-
-Meaning that by clicking on the display for each of the fruits, allows the user to ‘buy’ one of the fruits, at 
-market price, which will be deducted from the total cash. The user is not allowed to spend more than they have.
-
-The user will start with $100.
-
-Finally, style the whole experience with CSS!
-
-##Hard Mode
-Create a button below each of the Fruit buttons that allows the User to ‘sell’ one of their fruits of the same 
-type at the current market price. This will also remove one from their inventory. The user should be not able to 
-sell fruits they do not already own.
-
-##Pro Mode
-Limit the application experience to five minutes. At the end, stop the price fluctuation, sell all of the fruits 
-in their inventory at current market price, and then display the total money they earned from the experience. 
+1). User has five minutes to buy & sell from a list of four commodities(Apples,Oranges,Bananas & Grapes).
+2). The market price for each commodity is displayed on the screen once the application is loaded onto the browser.
+3). The Market price fluctuates between 1 and 50 cents from the existing price ever 15 seconds.
+4). The market price for a fruit should not go below the cost of 50 cents , or above the cost of $9.99 cents at any point during the 5 minutes interval.
+5). Average price of the fruit is calculated based on total cost of purchase divided by the existing current quantity.
+6). The user starts off with a credit of $100 in the account. User does not have the ability to buy a fruit if the cost is above cash available in the account.
+7). Selling a fruit removes it from the inventory. Total cost is deducted  based on current price of the fruit and added to the user account.
+8). At the end of five minute interval, all existing inventory is sold off at current market price and total profit/loss is displayed on the screen.
--- script.js
+++ script.js
@@ -1,196 +1,210 @@
-$(function(){
+$(function() {
 
-//startTime();
-console.log('inside jquery.....');
+    //startTime();
+    console.log('inside jquery.....');
 
-var totalCash=100;
-var timeCounter=0;
-// function Fruit(name,quantity,currentPrice,totalCost)
-var timeTracker=setInterval(timeUpdate,1000);
+    var totalCash = 100;
+    var timeCounter = 0;
+		var maxTime=30;
 
+    // timer
+    var timeTracker = setInterval(timeUpdate, 1000);
 
+    //function called ever second to increase clock time by 1 second
+    //Breaks setInterval function at the end of 300 seconds and calls calculateProfit function to calculate profit/loss
+    function timeUpdate() {
+        timeCounter++;
+        var time = $('.countdownContainer').find('span').text();
+        $('.countdownContainer').find('span').remove();
+        $('.countdownContainer').append('<span>Timer :: ' + timeCounter + '</span>');
 
-function timeUpdate(){
-  timeCounter++;
-	var time=$('.countdownContainer').find('span').text();
-	    $('.countdownContainer').find('span').remove();
-	   $('.countdownContainer').append('<span>Timer :: '+timeCounter+'</span>');
-
-	if(timeCounter==10){
-		  console.log('stopping newPriceGenerator loop');
-	    clearInterval(newPriceGenerator);
-      clearInterval(timeTracker);
-       calculateProfit();
-	 }
-}
-
-
-function calculateProfit(){
-var spentCash= 100 - totalCash;
-var stockSaleCash=0;
-var profit;
-for(var i=0;i<arr1.length;i++){
-	var calprice=Number(arr1[i].currentStockValue());
-	console.log("calprice: "+arr1[i]+'::'+calprice);
-stockSaleCash=Number(Number(stockSaleCash)+Number(arr1[i].currentStockValue())).toFixed(2);
-console.log("stock sale Cash: "+arr1[i]+'::'+stockSaleCash);
-}
-//console.log("spend Cash::"+spentCash);
-//console.log("stock sale Cash::"+stockSaleCash);
-profit=Number(stockSaleCash-spentCash).toFixed(2);
-$('.finalProfitDisplay').append('<span>Total Profit Made :: '+profit+'</span>');
-}
-
-// Building an object for each fruit.
-var banana=new Fruit('banana',randomStartPrice(),0,0);
-var apple=new Fruit('apple',randomStartPrice(),0,0);
-var orange=new Fruit('orange',randomStartPrice(),0,0);
-var grape=new Fruit('grapes',randomStartPrice(),0,0);
-
-// adding all  fruit objects created to an array
-var arr1 = [banana, apple, orange,grape];
-//console.log(arr1);
-//background-image: url("paper.gif");
-for(var i=0;i<arr1.length;i++){
-	var fruit='<div class="fruit"  id="'+arr1[i].name+'"><span>'+arr1[i].name+'</span></div>';
-	$('.fruits').append(fruit);
-	var priceDiv='<div  id="'+arr1[i].name+'"'+'class="priceDisplay">'+
-							'<button class="buy">Buy</button>'+
-							'<button class="sell">Sell</button>'+
-							'<span id="price'+arr1[i].name+'" data-'+arr1[i].name+'="'+arr1[i].currentPrice+'">'+arr1[i].currentPrice+'</span></div>';
-	$('.priceDisplays').append(priceDiv);
-}
-
-tableDisplay();
-updateTotalCash();
-
-$('.sell').on('click',function(){
-	console.log("user clicked on the sell buttn");
-	var fruit=$(this).parent().attr('id');
-	//var currentPrice=Number($(this).parent().find('span').text());
-	arr1.forEach(function(o){
-	//	console.log("inside looping function"+o.name);
-		if(o.name==fruit && o.quantity>0){
-			var price=Number(o.currentPrice);
-			console.log('price....'+price);
-			o.totalCost=Number(o.totalCost)-price;
-			console.log('total  cost...'+o.totalCost);
-     console.log('totalCash before calculation...'+totalCash+price);
-			totalCash=Number(Number(totalCash)+Number(price)).toFixed(2);
-			console.log('totalCash....'+totalCash);
-			o.quantity--;
-			updateTotalCash();
-			tableDisplay();
-			console.log(arr1);
-
-			}
-
-	});
+        if (timeCounter == maxTime) {
+            console.log('stopping newPriceGenerator loop');
+            clearInterval(newPriceGenerator);
+            clearInterval(timeTracker);
+            calculateProfit();
+        }
+    }
 
 
-})
+    // calculates profit at the end of the 300 seconds  and displays profit/loss on screen.
+    function calculateProfit() {
+        var spentCash = 100 - totalCash;
+        var stockSaleCash = 0;
+        var profit;
+        for (var i = 0; i < arr1.length; i++) {
+            //var calprice = Number(arr1[i].currentStockValue());
+            stockSaleCash = Number(Number(stockSaleCash) + Number(arr1[i].currentStockValue())).toFixed(2);
+        }
+        profit = Number(stockSaleCash - spentCash).toFixed(2);
+        $('.finalProfitDisplay').append('<span>Total Profit Made :: ' + profit + '</span>');
+    }
 
-$('.buy').on ('click', function() {
-	var fruit=$(this).parent().attr('id');
-  var currentPrice=Number($(this).parent().find('span').text());
-console.log('current price from data attribute'+currentPrice);
-//console.log(arr1);
-if(totalCash-currentPrice<=0){
+    // Building an object for each fruit.
+    var banana = new Fruit('banana', randomStartPrice(), 0, 0);
+    var apple = new Fruit('apple', randomStartPrice(), 0, 0);
+    var orange = new Fruit('orange', randomStartPrice(), 0, 0);
+    var grape = new Fruit('grapes', randomStartPrice(), 0, 0);
 
-	console.log('you have run out of cash');
-	runOutOfCashFunction();
-}else{
-	arr1.forEach(function(o){
-	//	console.log("inside looping function"+o.name);
-		if(o.name==fruit){
-			var price=Number(o.currentPrice);
-			o.totalCost=Number(o.totalCost)+price;
-			totalCash=Number(totalCash-price).toFixed(2);
-			o.quantity++
-			updateTotalCash();
-			tableDisplay();
-			console.log(arr1);
+    // adding all  fruit objects created to an array
+    var arr1 = [banana, apple, orange, grape];
 
-      }
 
-	});
+    for (var i = 0; i < arr1.length; i++) {
+        var fruit = '<div class="fruit"  id="' + arr1[i].name + '"><span>' + arr1[i].name + '</span></div>';
+        $('.fruits').append(fruit);
+        var priceDiv = '<div  id="' + arr1[i].name + '"' + 'class="priceDisplay">' +
+            '<button class="buy">Buy</button>' +
+            '<button class="sell">Sell</button>' +
+            '<span id="price' + arr1[i].name + '" data-' + arr1[i].name + '="' + arr1[i].currentPrice + '">' + arr1[i].currentPrice + '</span></div>';
+        $('.priceDisplays').append(priceDiv);
+    }
+
+    tableDisplay();
+    updateTotalCash();
+
+    $('.sell').on('click', function() {
+			 if(timeCounter<maxTime){
+        console.log("user clicked on the sell buttn");
+        var fruit = $(this).parent().attr('id');
+        //var currentPrice=Number($(this).parent().find('span').text());
+        arr1.forEach(function(o) {
+            //	console.log("inside looping function"+o.name);
+            if (o.name == fruit && o.quantity > 0) {
+                var price = Number(o.currentPrice);
+                console.log('price....' + price);
+                o.totalCost = Number(o.totalCost) - price;
+                console.log('total  cost...' + o.totalCost);
+                console.log('totalCash before calculation...' + totalCash + price);
+                totalCash = Number(Number(totalCash) + Number(price)).toFixed(2);
+                console.log('totalCash....' + totalCash);
+                o.quantity--;
+                updateTotalCash();
+                tableDisplay();
+                console.log(arr1);
+
+            }
+
+        });
 
 }
+    })
 
-});
+    $('.buy').on('click', function() {
+			  if(timeCounter<maxTime){
+        var fruit = $(this).parent().attr('id');
+        var currentPrice = Number($(this).parent().find('span').text());
+        console.log('current price from data attribute' + currentPrice);
+        //console.log(arr1);
+        if (totalCash - currentPrice <= 0) {
 
-function tableDisplay(){
-	$('.displayDetails').children().remove();
-for(var i=0;i<arr1.length;i++){
- var name=arr1[i].name;
- var quantity=Number(arr1[i].quantity);
- var totalCost=Number(arr1[i].totalCost);
-	var averagePrice=(isNaN(totalCost/quantity))?0:totalCost/quantity;
+            console.log('you have run out of cash');
+            runOutOfCashFunction();
+        } else {
+            arr1.forEach(function(o) {
+                //	console.log("inside looping function"+o.name);
+                if (o.name == fruit) {
+                    var price = Number(o.currentPrice);
+                    o.totalCost = Number(o.totalCost) + price;
+                    totalCash = Number(totalCash - price).toFixed(2);
+                    o.quantity++
+                        updateTotalCash();
+                    tableDisplay();
+                    console.log(arr1);
 
-	 var strBuilder='<tr id="'+name+'">'+
-	 								'<td>'+name+'</td>'+
-									'<td>'+quantity+'</td>'+
-									'<td>'+averagePrice+'</td>'
-    $('.displayDetails').append(strBuilder);
-	 console.log("name: "+name+"  quantity: "+quantity+" totalCost: "+totalCost);
+                }
 
-}
+            });
 
-}
+        }
+     }
+    });
 
-	var that=this;
-	console.log(that);
-	//var temp = 0;
-	var newPriceGenerator=setInterval(function(){
-		for(let i=0;i<arr1.length;i++){
-		var temp='#'+'price'+arr1[i].name;
-		$(that).find(temp).remove();
-		var randomNumber = Number(generateRandomPrice());
-		arr1[i].currentPrice=Number(Number(arr1[i].currentPrice)+Number(randomNumber)).toFixed(2);
-		 var newPriceDiv='<span id="price'+arr1[i].name+'"  data-'+arr1[i].name+'="'+arr1[i].currentPrice+'">'+arr1[i].currentPrice+'</span>';
-		 var div='.priceDisplays #'+arr1[i].name;
-		 console.log(div);
-		 $(div).append(newPriceDiv);
+    function tableDisplay() {
+        $('.displayDetails').children().remove();
+        for (var i = 0; i < arr1.length; i++) {
+            var name = arr1[i].name;
+            var quantity = Number(arr1[i].quantity);
+            var totalCost = Number(arr1[i].totalCost);
+            var averagePrice = Number((isNaN(totalCost / quantity)|| quantity==0) ? 0 : totalCost / quantity).toFixed(2);
 
-		 //clearInterval(timer);
-	}
-},5000)
+            var strBuilder = '<tr id="' + name + '">' +
+                '<td>' + name + '</td>' +
+                '<td>' + quantity + '</td>' +
+                '<td>' + averagePrice + '</td>'
+            $('.displayDetails').append(strBuilder);
+            console.log("name: " + name + "  quantity: " + quantity + " totalCost: " + totalCost);
 
-function updateTotalCash(){
-	$('#totalCash').find('span').remove();
-  $('#totalCash').append('<span>'+totalCash+'</span>');
-}
+        }
 
-function runOutOfCashFunction(){
-	$('#totalCash').append('<div  id="notEnoughCash"><span>Not Enough Cash  in the Bank</span></div>');
-}
+    }
 
-})   //end of document ready function.....
+    var that = this;
+    var newPriceGenerator = setInterval(function() {
+        for (let i = 0; i < arr1.length; i++) {
+            var temp = '#' + 'price' + arr1[i].name;
+            $(that).find(temp).remove();
+            var randomNumber = Number(generateRandomPrice(arr1[i].currentPrice));
+            arr1[i].currentPrice = Number(randomNumber).toFixed(2);
+            var newPriceDiv = '<span id="price' + arr1[i].name + '"  data-' + arr1[i].name + '="' + arr1[i].currentPrice + '">' + arr1[i].currentPrice + '</span>';
+            var div = '.priceDisplays #' + arr1[i].name;
+            console.log(div);
+            $(div).append(newPriceDiv);
+        }
+    }, 5000)
+
+    function updateTotalCash() {
+        $('#totalCash').find('span').remove();
+				$('.cashDisplay').find('#notEnoughCash').remove();
+        $('#totalCash').append('<span>' + totalCash + '</span>');
+    }
+
+    function runOutOfCashFunction() {
+			   $('.cashDisplay').find('#notEnoughCash').remove();
+        $('.cashDisplay').append('<div  id="notEnoughCash"><span>Not Enough Cash  in the Bank</span></div>');
+    }
+
+}) //end of document ready function.....
 
 
 //Generates random starting price for Fruit
 function randomStartPrice() {
-	return (Math.random() * (0.5 - 9.99) + 9.99).toFixed(2);
+	  var randomNumber=Math.random() * (0.5 - 9.99) + 9.99;
+		while(randomNumber<0.5 || randomNumber>9.99){
+			randomNumber=Math.random() * (0.5 - 9.99) + 9.99;
+		}
+    return Number(randomNumber).toFixed(2);
 }
 
 
-function generateRandomPrice() {
-	return (Math.random() * (-0.5 - 0.5) + 0.5).toFixed(2);
+function generateRandomPrice(currentPrice) {
+	var currentPrice=Number(currentPrice);
+  var randomNumber =Math.random() * (-0.5 - 0.5) + 0.5;
+	var newPrice=currentPrice+randomNumber;
+	while(newPrice<0.5 || newPrice>9.99){
+		randomNumber =Math.random() * (-0.5 - 0.5) + 0.5;
+		newPrice=currentPrice+randomNumber;
+	}
+	console.log('random number:: '+randomNumber);
+	console.log('current price :: '+currentPrice);
+	console.log('new  price :: '+newPrice);
+
+	return newPrice;
 }
 
 
-function Fruit(name,currentPrice,quantity,totalCost){
-			this.name=name;
-			this.quantity=quantity;
-			this.currentPrice=currentPrice;
-			this.totalCost=totalCost;
-			this.currentStockValue=function(){
-				var temp=Number(Number(this.quantity)*Number(this.currentPrice)).toFixed(2);
-				return temp=(temp!=NaN)? temp:0;
-			}
+function Fruit(name, currentPrice, quantity, totalCost) {
+    this.name = name;
+    this.quantity = quantity;
+    this.currentPrice = currentPrice;
+    this.totalCost = totalCost;
+    this.currentStockValue = function() {
+        var temp = Number(Number(this.quantity) * Number(this.currentPrice)).toFixed(2);
+        return temp = (temp != NaN) ? temp : 0;
+    }
 }
 
-function findFruit(obj,fruit){
-	console.log("inside findFruit function");
-	return obj.name==fruit;
+function findFruit(obj, fruit) {
+    console.log("inside findFruit function");
+    return obj.name == fruit;
 }
